dc4e97d03723e6e42355d50df8d481e2ec169a78
Romain Hennuyer <romainhk@gmail.com>
2010-08-27 11:46:07 +0200

    importation des 2 autres scripts

---
+++ DumZiBoT/statistics.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+"""
+Script to gather statistics for project importance and quality assessment.
+Updates the relevant project subpages with up-to-date stats.
+
+Copyright (c) 2008 - 2010 Nicolas Dumazet <nicdumz@gmail.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the MIT License.
+"""
+
+import urllib, re, cPickle
+from collections import defaultdict
+import pywikibot
+
+def putNb(page,nb):
+    try:
+        old = page.get()
+    except pywikibot.NoPage:
+        old = ''
+    except pywikibot.IsRedirectPage:
+        return
+    if old != str(nb):
+        page.put(str(nb))
+
+site = pywikibot.Site('fr', 'wikipedia')
+
+importance = ['maximum', u'élevée', 'moyenne', 'faible', 'inconnue']
+avancement = ['AdQ', 'A', 'BA', 'B', 'BD', u'ébauche', 'inconnu']
+patI = u"Catégorie:%s d'importance %s"
+patA = u"Catégorie:%s d'avancement %s"
+URLbase = u'http://toolserver.org/~bayo/intercat.php?'
+patURL = u'formCat1=%s d\'avancement %s&formCat2=%s d\'importance %s'
+
+pywikibot.setAction(u'Mise à jour des statistiques WP 1.0')
+
+pywikibot.Page(pywikibot.Link('Utilisateur:DumZiBoT/DateEval')).put('{{subst:CURRENTDAY}} {{subst:CURRENTMONTHNAME}} {{subst:CURRENTYEAR}}')
+
+param = u'{{[Uu]tilisateur:DumZiBoT/Demande statistiques projet[^}]*\|%s\s*=\s*([^\|}]*)\s*(?:\|[^}]*|)}}'
+reNom = re.compile(param % '[Nn]om')
+rePre = re.compile(param % u'[pP]réfixe')
+reType = re.compile(param % '[Tt]ype')
+
+reTl = re.compile(u'<!-- BEGIN BOT SECTION .* END BOT SECTION -->', re.DOTALL)
+
+resultats = {}
+pywikibot.output(u'Récupération des modèles...')
+reqs = pywikibot.Page(site, u'Utilisateur:DumZiBoT/Demande statistiques projet')
+gen = site.page_embeddedin(reqs, namespaces=[102])
+gen = site.preloadpages(gen, groupsize=100)
+for req in gen:
+    try:
+        text = req.get()
+    except pywikibot.Error:
+        continue
+    s = reNom.search(text)
+    if not s:
+        pywikibot.output(u'Pas de modèle sur %s' % req.title(asLink=True))
+        continue
+    projet = s.group(1).strip()
+    if ">" in projet or "<" in projet:
+        pywikibot.warning(u're buggy? %s / %s' % (projet, req.title(asLink=True)))
+        continue
+
+    pywikibot.output(u'Traitement du projet %s...' % projet)
+    s = rePre.search(text)
+    prefixe = s.group(1).strip()
+    s = reType.search(text)
+    type = s.group(1).strip()
+
+    pos = s.end()
+    if not resultats.has_key(projet):
+        # keys: category (avancement cat, or importance cat)
+        # values: Page set
+        artByCat = defaultdict(set)
+        # keys: article title
+        # values: [importance, avancement] 2-element list
+        articles = defaultdict(lambda: [None, None])
+
+        saveEval = pywikibot.config.datafilepath('eval', u'%s.dat' % projet)
+        try:
+            f = open(saveEval, 'r')
+            oldArticles = cPickle.load(f)
+            f.close()
+        except (IOError, EOFError):
+            oldArticles = {}
+
+        # XXX it would be nicer to use an appropriate Evaluation class instead
+        #   of a dict: it would allow cleaner semantics
+        resultats[projet] = {}
+
+        resultats[projet]['projet'] = projet
+
+        total = 0
+        for i, imp in enumerate(importance):
+            catname = patI % (prefixe, imp)
+            cat = pywikibot.Page(site, catname)
+            lenCat = 0
+            for p in site.categorymembers(cat, namespaces=[1]):
+                artByCat[imp].add(p)
+                lenCat += 1
+                articles[p.title()][0] = imp
+            resultats[projet][43 + i] = u'[[:%s|{{formatnum:%s}}]]' % (catname, lenCat)
+            total += lenCat
+
+        resultats[projet]['total'] = total
+
+        t = pywikibot.Page(pywikibot.Link(u'Projet:%s/Total' % projet))
+        putNb(t, total)
+
+        totalEval = total - len(artByCat[importance[-1]])
+        t = pywikibot.Page(pywikibot.Link(u'Projet:%s/Évaluation/Total évalué' % projet))
+        putNb(t, totalEval)
+
+        resultats[projet][48] = '{{formatnum:%s}}' % totalEval
+
+        for i, av in enumerate(avancement):
+            catname = patA % (prefixe, av)
+            cat = pywikibot.Page(site, catname)
+            for p in site.categorymembers(cat, namespaces=[1]):
+                artByCat[av].add(p)
+                try:
+                    articles[p.title()][1] = av
+                except KeyError:
+                    pass
+            resultats[projet][6*(i + 1)] = u'[[:%s|{{formatnum:%s}}]]' % (catname, len(artByCat[av]))
+
+
+        if isinstance(prefixe, unicode):
+            prefixe = urllib.quote(prefixe.encode('utf-8'))
+        i = 1
+        for av in avancement:
+            if isinstance(av, unicode):
+                a = urllib.quote(av.encode('utf-8'))
+            else:
+                a = av
+            for imp in importance:
+                if isinstance(imp, unicode):
+                    im = urllib.quote(imp.encode('utf-8'))
+                else:
+                    im = imp
+                inter = len(artByCat[av].intersection(artByCat[imp]))
+                if inter:
+                    url = URLbase + patURL % (prefixe, a, prefixe, im)
+                    url = url.replace(' ', '%20')
+                    resultats[projet][i] = u'<span class="plainlinks">[%s {{formatnum:%s}}]</span>' % (url, inter,)
+                else:
+                    resultats[projet][i] = ''
+                i += 1
+            i += 1
+
+### This is unfinished/untested feature "history" of evaluations
+#        oldKeys = set(oldArticles.keys())
+#        newKeys = set(articles.keys())
+#
+#        inter = oldKeys & newKeys
+#        new = newKeys - oldKeys
+#        deleted = oldKeys - newKeys
+
+        f = open(saveEval, 'w')
+        cPickle.dump(dict(articles), f)
+        f.close()
+
+    tl = u'<!-- BEGIN BOT SECTION -->'
+    if type.lower() == 'simple':
+        tl += u'{{Utilisateur:NicDumZ/Eval\n'
+    else:
+        tl += u'{{Utilisateur:NicDumZ/Eval Detail\n'
+
+    for (k,v) in resultats[projet].iteritems():
+        tl += u'| %s = %s\n' % (k, v)
+    tl += '}}\n<!-- END BOT SECTION -->'
+
+    (new_text, n) = reTl.subn(tl, text, 1)
+    if not n:
+        new_text=text[:pos] + '\n' + tl + text[pos:]
+
+    if new_text != text:
+        req.put(new_text)
---
+++ DumZiBoT/subpage_mover.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8  -*-
+"""
+Script moving special subpages when a page has been moved.
+It is supposed to run at regular intervals, as it only checks the
+last moves in ]now-delta, now]
+
+Copyright (c) 2008 - 2010 Nicolas Dumazet <nicdumz@gmail.com>
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the MIT License.
+"""
+
+import pywikibot
+from datetime import timedelta
+import re
+
+interval = timedelta(hours=2)
+subpages = ['Suppression', u'Article de qualité', u'Bon article',
+            'Droit d\'auteur', u'Neutralité', 'Traduction',
+            '[Aa]rchives?\d*', u'À faire']
+subpagesGroup = '(' + '|'.join(subpages) + ')'
+
+site = pywikibot.Site('fr', 'wikipedia')
+summary = u'Robot: renommage différé des sous-pages de discussion, suite au renommage de la page principale %s'
+
+copyvio = re.compile('(?i).*/(copyvio|purge|clean)')
+
+msg = {
+    'purge' : u'* [[Image:Commons-emblem-copyright.svg|20px]] %s [[Image:pfeil rechts.svg|12px]] %s',
+    'miss': u'** [[Fichier:Commons-emblem-query.svg|20px]] %s n\'existe plus. Sous-pages de %s non vérifiées',
+    'move': u'** Déplacement: %s [[Image:Progress axis arrow 2.svg|30px]] %s',
+    'user': u'** [[Fichier:Ambox contradict.svg|20px]] %s est une page utilisateur. Tentative de renommage de compte ?',
+    'error': u'** [[Fichier:Gnome-emblem-important.svg|20px]] %s : %s',
+    'ignore': u'** [[Fichier:Commons-emblem-notice.svg|20px]] Sous-page ignorée: %s'
+}
+
+bureaucrates = ['Anthere', 'Clem23', u'Céréales Killer', 'EDUCA33E', 'Esprit Fugace', 'Popo le Chien']
+
+section = re.compile('==[^=]*==[^=]*(?!=)', re.M)
+
+def getEligibleMoves():
+    now = site.getcurrenttime()
+    checkfrom = now - interval
+    checkto = checkfrom - interval
+    return site.logevents(logtype='move', \
+                            start=checkfrom, \
+                            end=checkto)
+log = {}
+for entry in getEligibleMoves():
+    title = entry.title()
+    if not title.isTalkPage():
+        log[entry] = []
+        if entry.ns() == 2 and not '/' in title.title():
+            # Tried to rename ?
+            if entry.user() not in bureaucrates:
+                log[entry].append(msg['user'] % title.title(asLink=True))
+
+        target = entry.new_title()
+        if copyvio.match(target.title()) or copyvio.match(title.title()):
+            log[entry] = msg['purge'] % (title.title(asLink=True), target.title(asLink=True))
+            continue
+
+        if not target.exists():
+            log[entry].append( msg['miss'] \
+                               % (target.title(asLink=True), \
+                                  title.title(asLink=True)))
+            continue
+        talk = title.toggleTalkPage()
+        targettalk = target.toggleTalkPage()
+        subpagesRe = re.compile(u'%s(?P<sub>/%s)' % (re.escape(talk.title()), subpagesGroup))
+        pywikibot.output(u'* %s -> %s' % (title.title(), target.title()))
+        for page in site.allpages(namespace=entry.ns()+1, \
+                                    prefix=talk.title(withNamespace=False) + '/'):
+            m = subpagesRe.match(page.title())
+            if m:
+                pywikibot.output(u'** %s' % page.title())
+                try:
+                    log[entry].append(msg['move'] % \
+                        (page.title(asLink=True), u'[[' + targettalk.title() + m.group('sub') + ']]'))
+                    site.movepage(page, targettalk.title() + m.group('sub'), \
+                        summary % targettalk.title(), \
+                        noredirect=entry.suppressedredirect())
+                except pywikibot.Error, e:
+                    log[entry].append( msg['error']% (page.title(asLink=True), e))
+            else:
+                log[entry].append(msg['ignore'] % page.title(asLink=True))
+
+logtext = ''
+for entry, events in log.iteritems():
+    if not events:
+        continue
+    if not isinstance(events, unicode):
+        logtext += u'* %s [[Image:pfeil rechts.svg|12px]] %s\n' % (entry.title().title(asLink=True), entry.new_title().title(asLink=True))
+        logtext += '\n'.join(events) + '\n'
+    else:
+        logtext += events + '\n'
+
+#logtext = u'* test (Tentative de ne laisser que 24 entrées au maximum)'
+if logtext:
+    logtext = '\n\n== ~~~~~ ==\n' + logtext
+    logpage = pywikibot.Page(site, 'Utilisateur:DumZiBoT/Log/MoveSubPages')
+    text = logpage.get()
+
+    allsections = [m for m in section.finditer(text)]
+    begin = allsections[0].start()
+    text = text[:begin].rstrip()
+    # keep only the 23 latest sections
+    keep = allsections[-23:]
+    for m in keep:
+        text += '\n' + m.group()
+    try:
+        logpage.put(text + logtext, 'Journalisation')
+    except pywikibot.Error, e:
+        print logtext
+        print e
