f32ef6d737659de25d9901594c5d568c58cd8d95
Romain Hennuyer <romainhk@gmail.com>
2011-01-11 13:11:50 +0100

    subject : semaine et numéro

--- BeBot/mail_wikimag.py
+++ BeBot/mail_wikimag.py
@@ -18,6 +18,8 @@ port=
 from=           # adresse de l'expédieur, truc@toto.fr
 motdepasse=
 #utilisateur=    # (facultatif) nom du compte sur le serveur smtp si différent du from
+
+        TODO : * gérer les interwiki/interlangue
     """
     def __init__(self, site, fichier_conf):
         self.site = site
@@ -27,12 +29,13 @@ motdepasse=
         date = datetime.date.today()
         self.lundi = date - datetime.timedelta(days=date.weekday())
         self.lundi_pre = self.lundi - datetime.timedelta(weeks=1)
+        self.semaine = self.lundi_pre.strftime("%W").lstrip('0')
         self.mag = pywikibot.Page(site, u'Wikipédia:Wikimag/%s/%s' % \
-                (self.lundi_pre.strftime("%Y"),
-                 self.lundi_pre.strftime("%W").lstrip('0') ) )
+                (self.lundi_pre.strftime("%Y"), self.semaine) )
+        self.numero = 0
         if self.mag.isRedirectPage():
             self.mag = self.mag.getRedirectTarget()
-        self.jocker = u'%$!' #Pour les liens http
+        self.jocker = u'%$!' #Pour repérer les liens http
         self.ajocker = BeBot.reverse(self.jocker)
 
     def url_(self, match):
@@ -49,6 +52,11 @@ motdepasse=
         except:
             pywikibot.error(u"Impossible d'effectuer la substitution")
             sys.exit(2)
+        #Numéro du mag
+        num = re.compile(u"\|numéro *= *(\d+)", re.LOCALE|re.UNICODE)
+        m = num.search(pagetmp.text)
+        if m is not None:
+            self.numero = m.group(1)
 
         text = pywikibot.Page(self.site, self.tmp).text
         r = re.compile(u"<br[ /]*>", re.LOCALE)
@@ -71,14 +79,14 @@ motdepasse=
         text = r.sub(r'\2 ( %shttp://fr.wikipedia.org/wiki/\1%s )' % ( self.jocker, self.ajocker), text)
         r = re.compile("\[\[([^\]]+)\]\]", re.LOCALE|re.UNICODE)
         text = r.sub(r'%shttp://fr.wikipedia.org/wiki/\1%s' % ( self.jocker, self.ajocker), text)
-        #Modification des liens http
+        #Modification des liens http ( -> url_() )
         r = re.compile("%s(.*?)%s" % ( re.escape(self.jocker), re.escape(self.ajocker)), re.LOCALE|re.UNICODE)
         text = r.sub(self.url_, text)
         #pywikibot.output(text)
 
         conf = BeBot.fichier_conf(self.conf_mail)
         if 'from' not in conf or 'mailinglist' not in conf or 'serveur' not in conf or 'port' not in conf:
-            pywikibot.warning(u"fichier de configuration incomplet")
+            pywikibot.error(u"fichier de configuration incomplet")
             sys.exit(3)
         if 'utilisateur' not in conf:
             conf['utilisateur'] = conf['from'].split('@', 1)[0]
@@ -89,8 +97,9 @@ motdepasse=
         msg['From'] = conf['from']
         msg['To'] = conf['mailinglist']
         msg['Date'] = formatdate(localtime=True)
-        msg['Subject'] = u'Wikimag du %s' % \
-                unicode(self.lundi_pre.strftime("%e/%m/%Y - semaine %W"), "utf-8")
+        msg['Subject'] = u'#%s, semaine %s - %s' % \
+                (self.numero, self.semaine, \
+                 self.lundi_pre.strftime("%e %b %Y").lstrip(' ') )
         try:
             smtp = smtplib.SMTP(conf['serveur'], conf['port'])
             smtp.starttls()
@@ -99,6 +108,7 @@ motdepasse=
             smtp.quit()
         except smtplib.SMTPException, mssg:
             print mssg
+        #pywikibot.output(msg.as_string())
 
 def main():
     if len(sys.argv) > 1:
