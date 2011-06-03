65cf5c770125c510b02b313201c227afa70d6939
Romain Hennuyer <romainhk@gmail.com>
2011-06-03 09:16:12 +0200

    test grandeur nature

--- BeBot/traduit_de.py
+++ BeBot/traduit_de.py
@@ -13,11 +13,12 @@ class TraduitDe:
         self.site = site
         self.TD = pywikibot.Page(self.site, u'Modèle:Traduit de')
         self.tdRE = re.compile("\{\{Traduit de\|([^\}]+)\}\}", re.LOCALE|re.IGNORECASE|re.UNICODE)
-        #self.tdRE = re.compile("\{\{Traduit de\|\w{2,5}(\|[^\|\}]*)*\}\}", re.LOCALE|re.IGNORECASE)
         self.oldidRE = re.compile("^\d+$", re.LOCALE|re.UNICODE|re.MULTILINE)
-        self.sites = {}
+        self.sites = {} # Mémorisation des sites explorés
 
     def dateRev(self, page, lang, title, oldid):
+        """ Donne la date de la version d'une page
+        """
         if lang not in self.sites.keys():
             self.sites[lang] = pywikibot.Site(lang)
         p = pywikibot.Page(self.sites[lang], title)
@@ -25,8 +26,8 @@ class TraduitDe:
         try:
             q = p.getVersionHistory()
         except:
-            pywikibot.output(u'impossible de récupérer la page %s:%s' % (lang, title))
-            # TODO : prendre la redirection , ou l'interwiki de page
+            pywikibot.output(u'* [[%s]] %s:%s' % (page.title(), lang, title))
+            # TODO : prendre la redirection, ou l'interwiki de page
             return r
         oldid = int(oldid)
         for i in q:
@@ -37,8 +38,8 @@ class TraduitDe:
 
     def run(self):
         founds = {}
-        pg = pywikibot.pagegenerators.ReferringPageGenerator(self.TD, followRedirects=False, withTemplateInclusion=True, onlyTemplateInclusion=False, step=150, total=50000, content=False)
-        #pg = pywikibot.pagegenerators.LinkedPageGenerator(pywikibot.Page(self.site, u'Utilisateur:BeBot/Test'), step=None, total=None, content=True)
+        #pg = pywikibot.pagegenerators.ReferringPageGenerator(self.TD, followRedirects=False, withTemplateInclusion=True, onlyTemplateInclusion=False, step=150, total=50000, content=False)
+        pg = pywikibot.pagegenerators.LinkedPageGenerator(pywikibot.Page(self.site, u'Utilisateur:BeBot/Test'), step=None, total=None, content=True) # Page de test
         for p in pywikibot.pagegenerators.DuplicateFilterPageGenerator(pg):
             if p.isTalkPage():
                 c = self.tdRE.search(p.text)
@@ -47,16 +48,19 @@ class TraduitDe:
                     a = d.split('|')
                     if len(a) > 2:
                         if self.oldidRE.match(a[2]):
-                            #founds[p.title()] = u''
                             date = self.dateRev(p, a[0], a[1], a[2])
                             if not date == u'':
-                                #date = datetime.date(int(date[0:4]),int(date[5:7]),int(date[8:10]))
-                                #td = u'{{Traduit de|%s|%s|%s|%s}}' % (a[0], a[1].strip(' '), date.strftime("%d/%m/%Y"), a[2])
-                                td=u''
+                                date = datetime.date(int(date[0:4]),int(date[5:7]),int(date[8:10]))
+                                fr = u''
+                                if len(a) > 4:
+                                    fr = u'|' + u'|'.join(a[3:5])
+                                td = u'{{Traduit de|%s|%s|%s|%s%s}}' % (a[0], a[1].strip(' '), date.strftime("%d/%m/%Y"), a[2], plus)
                                 founds[p.title()] = td
-                        else:
-                            pass
-        pywikibot.output(founds.keys())
+                                pywikibot.showDiff(p.text, self.tdRE.sub(td, p.text))
+                                #p.text = self.tdRE.sub(td, p.text)
+                                #p.save(comment=u'Ajout du paramètre date au modèle "Traduit de" ([[Discussion modèle:Traduit de#Date de traduction sans oldid]])', minor=False)
+        #for k in founds.iterkeys():
+        #    pywikibot.output(k)
         pywikibot.output(len(founds))
 
 def main():
