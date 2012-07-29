e49906f72caa8fd3cc92de0c190fa37366db53a6
romainhk <romainhk@gmail.com>
2012-07-29 23:13:22 +0200

    debug mode ; clore_trad ; retirer_modele

--- BeBot/Trad_maintenance.py
+++ BeBot/Trad_maintenance.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8  -*-
-import re, datetime, locale
+import re, datetime, locale, sys
 import BeBot
 import pywikibot
 locale.setlocale(locale.LC_ALL, '')
@@ -11,32 +11,53 @@ class Trad_maintenance:
       * supprimmer les anciennes demandes sans suite
       * clôre les relectures anciennes
     """
-    def __init__(self, site):
+    def __init__(self, site, debug):
         self.site = site
+        self.debug = debug
         self.resume = u"Maintenance du [[Projet:Traduction]]"
         self.date = datetime.date.today()
-        self.re_trad = re.compile(r'{{Traduction}}', re.IGNORECASE)
-        self.re_appel = re.compile('[\[\{]{2}Discussion:\w+?/Traduction[\]\}]{2}', re.LOCALE|re.UNICODE)
-        self.re_statut = re.compile('\|\s*status\s*=\s*(\d{1})', re.LOCALE|re.UNICODE)
+        self.re_trad = re.compile(r'{{Traduction}}\s*', re.IGNORECASE)
+        self.re_appel = re.compile('[\[\{]{2}Discussion:[\w/ ]+?/Traduction[\]\}]{2}\s*', re.LOCALE|re.UNICODE|re.IGNORECASE)
+        self.re_statut = re.compile('\|\s*status\s*=\s*(\d{1})', re.LOCALE|re.UNICODE|re.IGNORECASE)
+        
+    def retirer_le_modele_Traduction(page):
+        """ Supprime le {{Traduction}} d'une page
+        """"
+        b.text = self.re_trad.sub(r'', b.text)
+        pywikibot.output(u"&&&& retirer_le_modele_Traduction : %s" % page.title())
+        pywikibot.output(b.text)
+        # save b
         
     def supprimer(self, page, backlinks):
-    """ Supprime les pages voulues et nettoie les pages liées
-    """
+        """ Supprime la page et nettoie les pages liées
+        """
+        pywikibot.output(u"&& supprimer : %s" % page.title())
         pywikibot.output(backlinks)
         for b in backlinks:
             b.text = self.re_trad.sub(r'', b.text)
             b.text = self.re_appel.sub(r'', b.text)
-            pywikibot.output(b.title())
             pywikibot.output(b.text)
             # save b
+        self.retirer_le_modele_Traduction(BeBot.togglePageTrad(page))
         #delete page
         
     def clore_traduction(self, page):
         """ Clôt une relecture : statut 3/4 -> 5
         """
-        pass
+        pywikibot.output(u"&&& clore : %s" % page.title())
+        page.text = self.re_statut.sub('|status=5', page.text)
+        pywikibot.output(page.text)
+        # save page
+        self.retirer_le_modele_Traduction(BeBot.togglePageTrad(page))
 
+    def traduit_de(self, page):
+        """ Ajoute le {{Traduit de}} à la page de discussion
+        """
+        pass
+        
     def run(self):
+        if self.debug:
+            pywikibot.output(u"=== Mode débuggage actif ; aucunes modifications effectuées")
         ancien = self.date.year - 2
         re_annee = re.compile('Traduction de (\d{4})')
         parannee = pywikibot.Category(self.site, u"Catégorie:Traduction par année")
@@ -47,7 +68,7 @@ class Trad_maintenance:
                 continue
             annee = int(m.group(1))
             if annee <= ancien: # critère d'ancienneté
-                for p in c.articles(total=1, content=True):
+                for p in c.articles(total=4, content=True):
                     supprimer = False
                     n = self.re_statut.search(p.text)
                     if not n:
@@ -65,15 +86,25 @@ class Trad_maintenance:
                                 continue
                             else:
                                 backlinks.append(b)
-                    if n == 3 or n == 4:
+                    elif n == 3 or n == 4:
                         self.clore_traduction(p)
+                        
                     if supprimer:
-                        self.supprimer(p, backlinks)
-                        ## Vérifier aussi les licences
+                        if not self.debug:
+                            self.supprimer(p, backlinks)
+                            ## Vérifier aussi les licences : self.traduit_de(p)
+                        else:
+                            pywikibot.output(u"* [[%s]]" % p.title())
 
 def main():
+    debug = False
+    if len(sys.argv) == 1:
+        debug = True
+    elif len(sys.argv) != 0:
+        pywwikibot.output(u"Nombre de paramètres incorrectes")
+        sys.exit(1)
     site = pywikibot.getSite()
-    tm = Trad_maintenance(site)
+    tm = Trad_maintenance(site, debug)
     tm.run()
 
 if __name__ == "__main__":
