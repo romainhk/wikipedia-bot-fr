46c8465e07aa675b1dbd7af9e8cb27dd41febccf
romainhk <romainhk@gmail.com>
2013-04-23 22:27:40 +0200

    nazification de la reg

--- BeBot/supprimer_modele.py
+++ BeBot/supprimer_modele.py
@@ -13,7 +13,7 @@ if len(sys.argv) != 2:
     pywikibot.warning(u"Nombre de paramètres incorrectes")
     sys.exit(1)
 nom = sys.argv[1]
-renom = re.compile('{{'+nom+'[^}]*?}}')
+renom = re.compile('{{'+nom+'[^}\|/][^}]*?}}', re.IGNORECASE)
 modele = pywikibot.Page(site, u'Modèle:'+nom)
 if not modele.exists():
     pywikibot.error(u"Le modèle {nom} n'existe pas".format(nom=nom))
@@ -23,8 +23,8 @@ if not modele.exists():
 for b in modele.getReferences(follow_redirects=False, onlyTemplateInclusion=True, content=True):
     a = renom.sub(r'', b.text)
     b.text = a
-    BeBot.save(b, commentaire=u'Retrait du modèle {{{nom}}}'.format(nom=nom), debug=False)
-BeBot.delete(b, u'Retrait du modèle {{{nom}}}'.format(nom=nom), debug=True)
+    BeBot.save(b, commentaire=u'Retrait du modèle {{ {nom} }}'.format(nom=nom), debug=False)
+BeBot.delete(b, u'Retrait du modèle {{ {nom} }}'.format(nom=nom), debug=True)
 
 """
 renom  = re.compile(u'\[\[Modèle:'+nom+'([^\}]*?)\]\]', re.UNICODE)
