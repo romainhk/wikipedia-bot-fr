5dc0df21f3ffabaa7069c3a99e1cd890570cc1fb
romainhk <romainhk@gmail.com>
2013-07-20 00:11:57 +0200

    output sur une page du wiki

--- BeBot/bot_activite.py
+++ BeBot/bot_activite.py
@@ -13,15 +13,18 @@ TODO:
 """
 
 site = pywikibot.getSite()
+p = pywikibot.Page(site, u'Utilisateur:BeBot/Activité des bots') # Page des résultats
 classement = {} # classement par nb de modifs
 last = {} # date de dernière modif
+lim_jours = 182 # nombre de jours à prendre en compte
 peuactifs = {} # bots peu actifs
+lim_peu = 10 # limite de modifications pour les bots peu actifs
 
 bots = pywikibot.Category(site, u'Catégorie:Wikipédia:Bot/Autorisé')
-for b in bots.articles(total=5):
-    nb, derniere = BeBot.userdailycontribs(site, b.title(), days=365)
+for b in bots.articles(total=10):
+    nb, derniere = BeBot.userdailycontribs(site, b.title(), days=lim_jours)
     nom = b.title(withNamespace=False)
-    if nb > 10:
+    if nb > lim_peu:
         classement[nom] = nb
         last[nom] = derniere
     else:
@@ -30,7 +33,12 @@ for b in bots.articles(total=5):
 classement = sorted(classement.items(), key=itemgetter(1), reverse=True)
 
 # Affichage des résultats
+t = u'{|class=\"wikitable sortable\"\n!Nom!!Contribs ces %i derniers jours' % lim_jours
 for nom, nb in classement:
-    pywikibot.output(u'%s : %i ; %s' % (nom, nb, last[nom]))
+    t = t + u'\n|-\n|{{u|%s}}||%d||%s' % (nom, nb, last[nom])
+t = t + u'\n|}'
 
-pywikibot.output(peuactifs)
+t = t + u'\n== Les bots peu actifs (moins de %d modifs) ==\n' % lim_peu
+t = t + peuactifs
+
+BeBot.save(p, commentaire='Recalculationnement du classement', debug=False)
