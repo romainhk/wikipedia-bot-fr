e7ffdb3bf33fed7993cafabe70d7bcc0eb1d19b6
romainhk <romainhk@gmail.com>
2013-07-29 13:33:11 +0200

    min

--- BeBot/admin/supprimer_modele.py
+++ BeBot/admin/supprimer_modele.py
@@ -12,17 +12,17 @@ site = pywikibot.getSite()
 nom = u"Demande de traduction"
 #renom = re.compile('{{'+nom+'}}', re.IGNORECASE|re.MULTILINE)
 renom = re.compile('{{'+nom+'\|?[^}/]*?}}', re.IGNORECASE|re.MULTILINE)
-#renom = re.compile('{{'+nom+'}}', re.IGNORECASE)
 modele = pywikibot.Page(site, u'Modèle:'+nom)
 if not modele.exists():
     pywikibot.error(u"Le modèle {nom} n'existe pas".format(nom=nom))
     sys.exit(2)
 
+debug = True
 # Retrait des transclusions
 for b in modele.getReferences(follow_redirects=False, onlyTemplateInclusion=True, content=True, total=9):
     a = renom.sub(r'', b.text)
-    pywikibot.output(BeBot.diff(b.text, a))
+    BeBot.diff(b.text, a)
     b.text = a
-    BeBot.save(b, commentaire=u'Retrait du modèle "{nom}"'.format(nom=nom), debug=True)
-BeBot.delete(modele, u'Retrait du modèle "{nom}"'.format(nom=nom), debug=True)
+    BeBot.save(b, commentaire=u'Retrait du modèle "{nom}"'.format(nom=nom), debug=debug)
+BeBot.delete(modele, u'Retrait du modèle "{nom}"'.format(nom=nom), debug=debug)
 
