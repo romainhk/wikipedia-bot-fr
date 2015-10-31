45624eda16ace91ba80fe719f7c2cac160cdd32f
romainhk <romainhk@gmail.com>
2015-10-31 22:03:22 +0100

    passage à python3 (suite)

--- BeBot/atom_labellisations.py
+++ BeBot/atom_labellisations.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8  -*-
 import re, datetime, locale, sys, sqlite3
-import webhelpers.feedgenerator as feedgenerator
+#import webhelpers.feedgenerator as feedgenerator
+import pyatom
 import BeBot
 import pywikibot
 locale.setlocale(locale.LC_ALL, '')
@@ -14,16 +15,22 @@ class Atom_Labellisations:
         self.site = site
         self.debug = debug
         self.ajd = datetime.datetime.today()
-        self.re_modele = re.compile(u"\{\{((Article[ _]de[ _]qualité|Bon[ _]article|Portail[ _]de[ _]qualité|Bon[ _]portail).+?\}\})", re.LOCALE|re.IGNORECASE|re.MULTILINE|re.DOTALL|re.UNICODE)
-        self.re_date = re.compile(u"([^ _]+)[ /-]([^\s]+)[ /-](\d+)", re.LOCALE|re.IGNORECASE|re.UNICODE)
-        self.cats = [u"Catégorie:Article de qualité", u"Catégorie:Bon article", u"Catégorie:Portail de qualité", u"Catégorie:Bon portail"]
+        self.re_modele = re.compile("\{\{((Article[ _]de[ _]qualité|Bon[ _]article|Portail[ _]de[ _]qualité|Bon[ _]portail).+?\}\})", re.LOCALE|re.IGNORECASE|re.MULTILINE|re.DOTALL|re.UNICODE)
+        self.re_date = re.compile("([^ _]+)[ /-]([^\s]+)[ /-](\d+)", re.LOCALE|re.IGNORECASE|re.UNICODE)
+        self.cats = ["Catégorie:Article de qualité", "Catégorie:Bon article", "Catégorie:Portail de qualité", "Catégorie:Bon portail"]
         # Le flux
         self.fp = open(fluxatom, 'w') # OUTPUT
-        self.feed = feedgenerator.Atom1Feed(
-            title=u"[WP:fr] Labellisations",
-            link=u"http://romainhk.hd.free.fr/labellisations.atom",
-            description=u"Les articles récemment labellisés (AdQ, BA, PdQ, BP) sur la wikipédia francophone.",
-            language=u"fr")
+        #self.feed = feedgenerator.Atom1Feed(
+        #    title="[WP:fr] Labellisations",
+        #    link="http://hosaka.hd.free.fr/labellisations.atom",
+        #    description="Les articles récemment labellisés (AdQ, BA, PdQ, BP) sur la wikipédia francophone.",
+        #    language="fr")
+        self.feed = pyatom.AtomFeed(
+            title="[WP:fr] Labellisations",
+            subtitle="Les articles récemment labellisés (AdQ, BA, PdQ, BP) sur la wikipédia francophone.",
+            feed_url="http://hosaka.hd.free.fr/labellisations.atom",
+            url="http://hosaka.hd.free.fr/",
+            author="BeBot")
 
         #DB
         try:
@@ -32,7 +39,7 @@ class Atom_Labellisations:
             pywikibot.output("Impossible d'ouvrir la base sqlite {0}".format(bddsqlite))
             exit(2)
         self.conn.row_factory = sqlite3.Row
-        self.nom_base = u'labels'
+        self.nom_base = 'labels'
 
     def ajouteralabdd(self, page):
         # Suppression d'un modèle chiant pour le calcul de date
@@ -53,50 +60,58 @@ class Atom_Labellisations:
                         mois = BeBot.moistoint(o.group(2))
                         annee = int(o.group(3))
                     except:
-                        pywikibot.error(u"Problème de conversion de la date '%s' sur [[%s]]" % (o.group(0), page.title()))
+                        pywikibot.error("Problème de conversion de la date '%s' sur [[%s]]" % (o.group(0), page.title()))
                         return False
                     date = datetime.datetime(annee, mois, jour)
                 else:
-                    pywikibot.error(u"Impossible de reconnaitre le format de date pour %s" % page.title())
+                    pywikibot.error("Impossible de reconnaitre le format de date pour %s" % page.title())
                     return False
             # Sauvegarde
             curseur = self.conn.cursor()
-            req = u'INSERT INTO %s ' % self.nom_base \
-                + u'(titre, label, date) VALUES ("%s", "%s", "%s")' \
+            req = 'INSERT INTO %s ' % self.nom_base \
+                + '(titre, label, date) VALUES ("%s", "%s", "%s")' \
                 % (page.title(), categorie, date)
             if self.debug:
-                pywikibot.output("Ajout de %s" % page.title())
+                #pywikibot.output("Ajout de %s" % page.title())
+                print('.',end="",flush=True)
                 return
             try:
                 curseur.execute(req)
             except sqlite3.Error as e:
-                pywikibot.error(u"Erreur lors de l'INSERT :\n%s" % (e.args[0]))
+                pywikibot.error("Erreur lors de l'INSERT :\n%s" % (e.args[0]))
             self.conn.commit()
             return True
         if page.namespace() == 0:
-            pywikibot.error(u"Impossible de trouver le modèle AdQ/BA sur l'article %s" % page.title())
+            pywikibot.error("Impossible de trouver le modèle AdQ/BA sur l'article %s" % page.title())
         return False
 
     def supprimerdelabdd(self, page):
         curseur = self.conn.cursor()
-        req = u'DELETE FROM %s ' % self.nom_base \
-            + u'WHERE titre="%s"' % page.title()
+        req = 'DELETE FROM %s ' % self.nom_base \
+            + 'WHERE titre="%s"' % page.title()
         if self.debug:
             pywikibot.output("Suppression de %s" % page.title())
             return
         try:
             curseur.execute(req)
         except sqlite3.Error as e:
-            pywikibot.error(u"Erreur lors du DELETE :\n%s" % (e.args[0]))
+            pywikibot.error("Erreur lors du DELETE :\n%s" % (e.args[0]))
         self.conn.commit()
         
     def ajouterauflux(self, page, date, categorie):
-        self.feed.add_item(
+        #self.feed.add_item(
+        #    title=page.title(),
+        #    link="http://fr.wikipedia.org/wiki/%s" % page.title(asUrl=True),
+        #    description=categorie,
+        #    pubdate=date,
+        #    categories=[categorie])
+        self.feed.add(
             title=page.title(),
-            link=u"http://fr.wikipedia.org/wiki/%s" % page.title(asUrl=True),
-            description=categorie,
-            pubdate=date,
-            categories=[categorie])
+            url="http://fr.wikipedia.org/wiki/%s" % page.title(asUrl=True),
+            content=categorie,
+            content_type="html",
+            author="BeBot",
+            updated=date)
 
     def run(self):
         adq_wp = []
@@ -126,7 +141,8 @@ class Atom_Labellisations:
             categorie = r['label']
             self.ajouterauflux(p, date, categorie)
         if not self.debug:
-            self.feed.write(self.fp, 'utf-8')
+            #self.feed.write(self.fp, 'utf-8')
+            self.fp.write(self.feed.to_string())
         pywikibot.output("Nombre de modifications sur la base : {0}".format(self.conn.total_changes))
         self.fp.close()
 
@@ -135,7 +151,7 @@ def main():
     if BeBot.blocage(site):
         sys.exit(7)
     debug = False
-    if unicode(len(sys.argv)) not in u"2 3":
+    if len(sys.argv) > 4 or len(sys.argv) < 3:
         pywikibot.error("Syntaxe: atom_labellisations.py BASE_SQLITE FLUX_ATOM [DEBUG]")
         sys.exit(1)
 
--- BeBot/infolettre_pddis.py
+++ BeBot/infolettre_pddis.py
@@ -20,63 +20,63 @@ class Infolettre:
         self.semaine = self.lundi_pre.isocalendar()[1]
         self.annee = self.lundi_pre.isocalendar()[0]
         self.abn = {
-                "wikimag" : u"Wikipédia:Wikimag/Abonnement",
-                "raw"     : u"Wikipédia:Regards sur l'actualité de la Wikimedia/Inscription" }
+                "wikimag" : "Wikipédia:Wikimag/Abonnement",
+                "raw"     : "Wikipédia:Regards sur l'actualité de la Wikimedia/Inscription" }
 
     def adl(self, mag):
         """ (Avec le wikimag) Ajoute les adq/ba de la semaine à l'Atelier de Lecture
         """
-        separation = u'<center><hr style="width:42%;" /></center>'
+        separation = '<center><hr style="width:42%;" /></center>'
 
         split = re.compile("\|([\w \xe9]+?)=", re.LOCALE|re.UNICODE|re.MULTILINE|re.DOTALL)
         lien = re.compile("\[\[.*?\]\]", re.LOCALE|re.UNICODE)
-        params = {  'adq': u'', u'propositions adq' : u'',
-                    'ba' : u'', u'propositions ba'  : u'' } # Les paramètres du mag
+        params = {  'adq': '', 'propositions adq' : '',
+                    'ba' : '', 'propositions ba'  : '' } # Les paramètres du mag
         a = re.split(split, mag.text)
         for i in range(1, len(a), 2):
             #retrait des liens
             a[i] = lien.sub(r'', a[i])
             params[a[i].lower()] = a[i+1].rstrip('\n').strip(' ')
 
-        lumiere = pywikibot.Page(self.site, u'Wikipédia:Atelier de lecture/Lumière sur...')
+        lumiere = pywikibot.Page(self.site, 'Wikipédia:Atelier de lecture/Lumière sur...')
         #Retrait des a-label
         alabel = re.compile("\{\{[aA]-label\|([^\}\|]+).*?\}\}", re.LOCALE|re.UNICODE)
         params['adq'] = alabel.sub(r'[[\1]]', params['adq'])
         params['propositions adq'] = alabel.sub(r'[[\1]]', params['propositions adq'])
         params['ba']  = alabel.sub(r'[[\1]]', params['ba'])
         params['propositions ba'] = alabel.sub(r'[[\1]]', params['propositions ba'])
-        propositions = params['propositions adq'] + u'\n' + params['propositions ba']
+        propositions = params['propositions adq'] + '\n' + params['propositions ba']
         #Ajout des icones
         icone = re.compile("^\* ", re.LOCALE|re.UNICODE|re.MULTILINE)
         params['adq'] = icone.sub(r'* {{AdQ|20px}} ', params['adq'])
         params['ba']  = icone.sub(r'* {{BA|20px}} ', params['ba'])
-        lumiere.text = u'[[Fichier:HSutvald2.svg|left|60px]]\nLes articles labellisés durant la semaine dernière et les nouvelles propositions au label.{{Clr}}\n' \
-                + u'\n{{colonnes|nombre=3|\n' + params['adq'] + u'\n}}\n' + separation \
-                + u'\n{{colonnes|nombre=3|\n' + params['ba']  + u'\n}}\n' + separation \
-                + u'\n{{colonnes|nombre=3|\n' + propositions  + u'\n}}\n' \
-                + u'<noinclude>\n[[Catégorie:Wikipédia:Atelier de lecture|Lumière sur...]]\n</noinclude>'
-        pywikibot.output(u"# Publication sur l'Atelier de lecture")
-        BeBot.save(lumiere, commentaire=u'Maj hebdomadaire de la liste', debug=self.debug)
+        lumiere.text = '[[Fichier:HSutvald2.svg|left|60px]]\nLes articles labellisés durant la semaine dernière et les nouvelles propositions au label.{{Clr}}\n' \
+                + '\n{{colonnes|nombre=3|\n' + params['adq'] + '\n}}\n' + separation \
+                + '\n{{colonnes|nombre=3|\n' + params['ba']  + '\n}}\n' + separation \
+                + '\n{{colonnes|nombre=3|\n' + propositions  + '\n}}\n' \
+                + '<noinclude>\n[[Catégorie:Wikipédia:Atelier de lecture|Lumière sur...]]\n</noinclude>'
+        pywikibot.output("# Publication sur l'Atelier de lecture")
+        BeBot.save(lumiere, commentaire='Maj hebdomadaire de la liste', debug=self.debug)
 
     def wikimag(self):
         """ Wikimag """
-        mag = pywikibot.Page(self.site, u'Wikipédia:Wikimag/%s/%s' % (self.annee, self.semaine) )
+        mag = pywikibot.Page(self.site, 'Wikipédia:Wikimag/%s/%s' % (self.annee, self.semaine) )
         if not mag.exists():
             pywikibot.output("# Impossible de trouver l'infolettre à distribuer")
             return ""
         if mag.isRedirectPage():
             mag = mag.getRedirectTarget()
         numero = '???'
-        num = re.compile(u"\|numéro *= *(\d+)", re.LOCALE|re.UNICODE)
+        num = re.compile("\|numéro *= *(\d+)", re.LOCALE|re.UNICODE)
         m = num.search(mag.text)
         if m is not None:
             numero = m.group(1)
 
-        self.resume = u'Demandez [[Wikipédia:Wikimag/%s/%s|Cannes Midi (n°%s)]]. Le tueur de Cannes frappe encore... 5 cents' \
+        self.resume = 'Demandez [[Wikipédia:Wikimag/%s/%s|Cannes Midi (n°%s)]]. Le tueur de Cannes frappe encore... 5 cents' \
                 % (self.annee, self.semaine, numero)
         # Message à distribuer
-        msg  = u"\n\n== Wikimag n°%s - Semaine %s ==\n" % (numero, self.semaine)
-        msg += u"{{Wikimag message|%s|%s|%s}} ~~~~" % (numero, self.annee, self.semaine)
+        msg  = "\n\n== Wikimag n°%s - Semaine %s ==\n" % (numero, self.semaine)
+        msg += "{{Wikimag message|%s|%s|%s}} ~~~~" % (numero, self.annee, self.semaine)
         # Travail pour l'atelier de lecture
         self.adl(mag)
         return msg
@@ -84,15 +84,15 @@ class Infolettre:
     def raw(self):
         """ Regards sur l'actualité de la Wikimedia """
         self.semaine = self.lundi.isocalendar()[1]
-        self.resume = u"Regards sur l'actualité de la Wikimedia - semaine %s de %s" % (self.semaine, self.annee)
-        msg  = u"\n\n== Regards sur l'actualité de la Wikimedia - Semaine %s ==\n" % (self.semaine)
-        msg += u"{{Regards sur l'actualité de la Wikimedia/PdD|%s|%s}}\n~~~~" % (self.annee, self.semaine)
+        self.resume = "Regards sur l'actualité de la Wikimedia - semaine %s de %s" % (self.semaine, self.annee)
+        msg  = "\n\n== Regards sur l'actualité de la Wikimedia - Semaine %s ==\n" % (self.semaine)
+        msg += "{{Regards sur l'actualité de la Wikimedia/PdD|%s|%s}}\n~~~~" % (self.annee, self.semaine)
         return msg
 
     def rm_old(self, page):
         """ Supprime les anciens mag
         """
-        if self.infolettre == u"wikimag":
+        if self.infolettre == "wikimag":
             oldmag = re.compile("^== Wikimag (n.?\d+ )?- Semaine (\d+) ?==.*?^==", re.LOCALE|re.UNICODE|re.MULTILINE|re.DOTALL)
         else:
             return False
@@ -117,16 +117,16 @@ class Infolettre:
         BeBot.save(lecteur, commentaire=self.resume, debug=self.debug)
 
     def run(self):
-        if   self.infolettre == u"wikimag":
+        if   self.infolettre == "wikimag":
             msg = self.wikimag()
-        elif self.infolettre == u"raw":
+        elif self.infolettre == "raw":
             msg = self.raw()
         else:
-            pywikibot.output(u"Infolettre '%s' inconnue. Abandon." % self.infolettre)
+            pywikibot.output("Infolettre '%s' inconnue. Abandon." % self.infolettre)
             sys.exit(1)
 
         # Liste des abonnés
-        r = re.compile(u"\*\* \{\{u\|(.+?)\}\}\s*(\{\{BeBot nopurge\}\})?", re.LOCALE|re.UNICODE|re.IGNORECASE)
+        r = re.compile("\*\* \{\{u\|(.+?)\}\}\s*(\{\{BeBot nopurge\}\})?", re.LOCALE|re.UNICODE|re.IGNORECASE)
         liste = [] # [ Nom d'utilisateur ; bool : purge des anciens ]
         for i in BeBot.page_ligne_par_ligne(self.site, self.abn[self.infolettre]):
             m = r.search(i)
@@ -139,7 +139,7 @@ class Infolettre:
         # Distribution
         if hasattr(self, "resume"):
             for l,p in liste:
-                boiteauxlettres = pywikibot.Page(self.site, u"Utilisateur:"+l).toggleTalkPage()
+                boiteauxlettres = pywikibot.Page(self.site, "Utilisateur:"+l).toggleTalkPage()
                 self.newsboy(boiteauxlettres, msg, p)
 
 def main():
